daeee442ed0d0b65935283f60fa6e9c9075c9ba3
Rosan009 <rosannarwal4@gmail.com>
2025-07-04 10:00:58 +0530

    fix: static build + react routing

--- Techlambdas-BackEnd/src/main/java/com/project/Techlambdas_BackEnd/config/WebConfig.java
+++ Techlambdas-BackEnd/src/main/java/com/project/Techlambdas_BackEnd/config/WebConfig.java
@@ -7,14 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/{spring:\\w+}")
+            .setViewName("forward:/index.html");
+    registry.addViewController("/**/{spring:\\w+}")
+            .setViewName("forward:/index.html");
+    registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+            .setViewName("forward:/index.html");
+  }
 }
+
 
